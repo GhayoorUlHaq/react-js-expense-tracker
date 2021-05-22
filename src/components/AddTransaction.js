c153fc097d2882aba4bd3fba40ff50a1d2bcaf1a
GhayoorUlHaq <ghayoor10th@gmail.com>
2021-05-22 16:42:44 +0500

    Functionality added

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {GlobalContext} from "../context/GlobalState";
 
 const AddTransaction = () => {
+    const {addTransaction} = useContext(GlobalContext)
+
+    const [text, setText] = useState()
+    const [amount, setAmount] = useState()
+
+    const onsubmit = (e) => {
+        e.preventDefault()
+        let transaction = {
+            id: Math.floor(Math.random()*10000000),
+            text,
+            amount: +amount,
+        }
+        addTransaction(transaction)
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onsubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..."/>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount"
                     >Amount <br/>
-                        (negative - expense, positive - income)</label
-                    >
-                    <input type="number" placeholder="Enter amount..."/>
+                        (negative - expense, positive - income)</label>
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..."/>
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {GlobalContext} from "../context/GlobalState";
 
 const TransactionList = () => {
+    const {transactions, deleteTransaction} = useContext(GlobalContext)
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-               <li className="minus">
-                  Cash <span>-$400</span><button className="delete-btn">x</button>
-               </li>
+
+                {
+                    transactions.map(transaction => {
+                        const sign = transaction.amount < 0 ? '-': '+';
+                        return (
+                        <li className={sign === '-' ? 'minus' : 'plus'}>
+                            {transaction.text} <span>{ sign + '$' + Math.abs(transaction.amount)}</span>
+                            <button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn">x</button>
+                        </li>
+                    )} )
+                }
+
+
             </ul>
         </>
     );
